b96d369cb7f22b4bff63f0802c7c53d50637634c
maryam-work <maryam.orders.in@gmail.com>
2026-01-28 23:40:39 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from './App';
 
@@ -12,8 +13,10 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
